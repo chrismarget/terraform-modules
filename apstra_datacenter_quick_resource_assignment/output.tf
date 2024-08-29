c65cb042b582b058d95440a69be64570e5cff4da
Chris Marget <5470536+chrismarget@users.noreply.github.com>
2024-08-29 08:23:31 -0400

    fix blueprint id, fix output

--- apstra_datacenter_quick_resource_assignment/output.tf
+++ apstra_datacenter_quick_resource_assignment/output.tf
@@ -1,4 +1,4 @@
-output "asn_pool_id" { value = apstra_asn_pool.rag.id }
-output "ipv4_pool_id" { value = apstra_ipv4_pool.rag.id }
-output "ipv6_pool_id" { value = apstra_ipv6_pool.rag.id }
-output "vni_pool_id" { value = apstra_vni_pool.rag.id }
+output "asn_pool_id" { value = length(apstra_asn_pool.rag) > 0 ? apstra_asn_pool.rag[0].id : null }
+output "ipv4_pool_id" { value = length(apstra_ipv4_pool.rag) > 0 ? apstra_ipv4_pool.rag[0].id : null }
+output "ipv6_pool_id" { value = length(apstra_ipv6_pool.rag) > 0 ? apstra_ipv6_pool.rag[0].id : null }
+output "vni_pool_id" { value = length(apstra_vni_pool.rag) > 0 ? apstra_vni_pool.rag[0].id : null }
--- apstra_datacenter_quick_resource_assignment/resource_pool_allocations.tf
+++ apstra_datacenter_quick_resource_assignment/resource_pool_allocations.tf
@@ -2,14 +2,14 @@
 
 resource "apstra_datacenter_resource_pool_allocation" "asn" {
   for_each     = local.asn_rags
-  blueprint_id = "82a4dde9-eb98-4666-a010-d82f66296be4"
+  blueprint_id = var.blueprint_id
   pool_ids     = [one(apstra_asn_pool.rag).id]
   role         = lookup(local.rosetta, each.value, each.value)
 }
 
 resource "apstra_datacenter_resource_pool_allocation" "ipv4" {
   for_each        = local.ipv4_rags
-  blueprint_id    = "82a4dde9-eb98-4666-a010-d82f66296be4"
+  blueprint_id = var.blueprint_id
   pool_ids        = [one(apstra_ipv4_pool.rag).id]
   role            = startswith(each.key, "rag_ip_sz:") ? regex("(?P<a>^sz:)(?P<b>.*,)(?P<role>.*$)", each.value)["role"] : lookup(local.rosetta, each.value, each.value)
   routing_zone_id = startswith(each.key, "rag_ip_sz:") ? regex("(?P<a>^sz:)(?P<rzid>.*)(?P<b>,.*$)", each.value)["rzid"] : null
@@ -17,7 +17,7 @@ resource "apstra_datacenter_resource_pool_allocation" "ipv4" {
 
 resource "apstra_datacenter_resource_pool_allocation" "ipv6" {
   for_each        = local.ipv6_rags
-  blueprint_id    = "82a4dde9-eb98-4666-a010-d82f66296be4"
+  blueprint_id = var.blueprint_id
   pool_ids        = [one(apstra_ipv6_pool.rag).id]
   role            = startswith(each.key, "rag_ipv6_sz:") ? regex("(?P<a>^sz:)(?P<b>.*,)(?P<role>.*$)", each.value)["role"] : lookup(local.rosetta, each.value, each.value)
   routing_zone_id = startswith(each.key, "rag_ipv6_sz:") ? regex("(?P<a>^sz:)(?P<rzid>.*)(?P<b>,.*$)", each.value)["rzid"] : null
@@ -25,7 +25,7 @@ resource "apstra_datacenter_resource_pool_allocation" "ipv6" {
 
 resource "apstra_datacenter_resource_pool_allocation" "vni" {
   for_each     = local.vni_rags
-  blueprint_id = "82a4dde9-eb98-4666-a010-d82f66296be4"
+  blueprint_id = var.blueprint_id
   pool_ids     = [one(apstra_vni_pool.rag).id]
   role         = lookup(local.rosetta, each.value, each.value)
 }
